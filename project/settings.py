11f4739272195b07bda6eef1165c9e00897920b6
Natalia Gálvez Moreno <9204887@alumnos.ufv.es>
2025-12-20 17:43:13 +0100

    Conexión con base de datos alojada en azure modificando settings.py

--- project/settings.py
+++ project/settings.py
@@ -81,24 +81,24 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
- #       "ENGINE": "django.db.backends.postgresql",
-  #      "NAME": "ngmdb",
-  #      "USER": "citus",
-  #      "PASSWORD": os.getenv("DB_PASSWORD"),
-  #      "HOST": "c-ngmrelecloudpg.wfrvmeihcyvdgs.postgres.cosmos.azure.com",
-  #      "PORT": "5432",
-  #      "OPTIONS": {"sslmode": "require"}
-  #  }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+       "ENGINE": "django.db.backends.postgresql",
+      "NAME": "ngmdb",
+      "USER": "citus",
+      "PASSWORD": os.getenv("DB_PASSWORD"),
+      "HOST": "c-ngmrelecloudpg.wfrvmeihcyvdgs.postgres.cosmos.azure.com",
+      "PORT": "5432",
+      "OPTIONS": {"sslmode": "require"}
+  }
 }
+
+#DATABASES = {
+ #   'default': {
+ #       'ENGINE': 'django.db.backends.sqlite3',
+ #       'NAME': BASE_DIR / 'db.sqlite3',
+ #   }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
